fd6a8baac49e344b9da3bd66a3ca528db5be1bff
bolyardk22 <collinsk22@mycu.concord.edu>
2017-06-20 16:53:12 -0400

    replaces multiples of 15 with 'mined minds' plus all the previous stuff

--- print_to_screen.rb
+++ print_to_screen.rb
@@ -1,10 +1,18 @@
+#finds numbers starting with 0 and going to 100
 count = 0
 while count < 100
+	#adds one each loop
 	count +=1
-	if count %3 ==0
+	#looks for multiples of 15 and replaces them with "mined minds"
+	if count %15 ==0
+		puts "mined minds"
+		# looks for multiples of 3 and replaces them with "mined"
+	elsif count %3 ==0
 		puts "mined"
+		#looks for multiples of 5 and replaces them with "minds"
 	elsif count %5 ==0
 		puts "minds"
+		#adds the other numbers in
 	else puts count
 	end
 end
