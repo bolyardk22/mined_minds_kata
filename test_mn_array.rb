ca6b9554a783791cec21d3781ba4eee6fce8cceb
bolyardk22 <collinsk22@mycu.concord.edu>
2017-07-07 14:55:18 -0400

    array tests all work

--- test_mn_array.rb
+++ test_mn_array.rb
@@ -14,4 +14,28 @@ class Mnarray_Test <Minitest::Test
 		assert_equal(100,results.count)
 	end
 
+	def test_indextwo_returns_mined
+		assert_equal("mined",results[2])
+	end
+
+	def test_indexfive_returns_mined
+		assert_equal("mined",results[5])
+	end
+
+	def test_indexfour_returns_minds
+		assert_equal("minds",results[4])
+	end
+
+	def test_indexnine_returns_minds
+		assert_equal("minds",results[9])
+	end
+
+	def test_indexfourteen_returns_mined_minds
+		assert_equal("mined minds",results[14])
+	end
+
+	def test_indextwentynine_returns_mined_minds
+		assert_equal("mined minds",results[29])
+	end
+
 end
